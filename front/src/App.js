1155448c2252c532ee443ff18c34b8f6551f137d
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-10-13 22:50:51 -0500

    INSTALACION REACT , ACTUALIZACION LOGO E ICONO
    INSTALACION REACT , ACTUALIZACION LOGO E ICONO
    SE TRABAJA FRONT

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de Mascotas Vety Shop</h1>
+      <img src="./images/VetyShop.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
