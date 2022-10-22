084690b851efb925f5c8ca0505dc141373ab168f
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-10-21 22:51:44 -0500

    Clase octubre21
    product details -ruta en apps

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import { Footer } from './components/Footer';
 import Home from './components/Home';
-
+import { ProductDetails } from './components/products/ProductDetails';
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -17,9 +17,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/Home" element={<Home />}/>
+            <Route path="/producto/:id" element={<ProductDetails />}/>
           </Routes>
         </div>
-
         <Footer/>
     </div>
     </Router>
