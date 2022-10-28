805de0a1fbcaedbfc686a8ed1afdf17fad1f523c
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-10-28 01:02:15 -0500

    se crea carpeta utils -middleware

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,72 +1,64 @@
-const producto=require("../models/productos")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const producto=require("../models/productos");
+const ErrorHandler = require("../utils/errorHandler");
 const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); //Usurpación del require
 
 //Ver la lista de productos
-exports.getProducts= async(req,res,next) =>{
+exports.getProducts=catchAsyncErrors(async (req,res,next) =>{
     const productos= await producto.find();
     if (!productos){
-        return res.status(404).json({
-            success:false,
-            error:true
-        })
+        return next(new ErrorHandler("Informacion no encontrada", 404))
     }
-    
+
     res.status(200).json({
         success:true,
         cantidad: productos.length,
         productos
-
     })
-}
+})
 
 //Ver un producto por ID
-exports.getProductById= async (req, res, next)=>{
+exports.getProductById= catchAsyncErrors( async (req, res, next)=>{
     const product= await producto.findById(req.params.id)
     
     if (!product){
-            return res.status(404).json({
-            success:false,
-            message: 'No encontramos ese producto',
-            error:true
-        })
-    }
+            return next(new ErrorHandler("Producto no encontrado", 404))
+        }
+    
     res.status(200).json({
         success:true,
         message:"Aqui debajo encuentras información sobre tu producto: ",
         product
     })
-}
+})
 
 //Update un producto
-exports.updateProduct= async (req,res,next) =>{
+exports.updateProduct= catchAsyncErrors(async (req,res,next) =>{
     let product= await producto.findById(req.params.id) //Variable de tipo modificable
-    if (!product){ //Verifico que el objeto no existe para finalizar el proceso
-            return res.status(404).json({
-            success:false,
-            message: 'No encontramos ese producto'
-        })
+    if (!product){
+        return next(new ErrorHandler("Producto no encontrado", 404))
     }
-//Si el objeto si existia, entonces si ejecuto la actualización
-product= await producto.findByIdAndUpdate(req.params.id, req.body, {
-    new:true, //Valido solo los atributos nuevos o actualizados
-    runValidators:true
+    
+    //Si el objeto si existia, entonces si ejecuto la actualización
+    product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, //Valido solo los atributos nuevos o actualizados
+        runValidators:true
     });
- //Respondo Ok si el producto si se actualizó
- res.status(200).json({
-    success:true,
-    message:"Producto actualizado correctamente",
-    product
-    })   
-}
+    //Respondo Ok si el producto si se actualizó
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+})
+
 
 //Eliminar un producto
-exports.deleteProduct= async (req,res,next) =>{
+exports.deleteProduct= catchAsyncErrors(async (req,res,next) =>{
     const product= await producto.findById(req.params.id) //Variable de tipo modificable
-    if (!product){ //Verifico que el objeto no existe para finalizar el proceso
-            return res.status(404).json({ //Si el objeto no existe, return termina el metodo
-            success:false,
-            message: 'No encontramos ese producto'
-        })
+   
+    if (!product){
+        return next(new ErrorHandler("Producto no encontrado", 404))
     }
 
     await product.remove();//Eliminamos el proceso
@@ -74,19 +66,17 @@ exports.deleteProduct= async (req,res,next) =>{
         success:true,
         message:"Producto eliminado correctamente"
     })
-}
-
+})
 
 //Crear nuevo producto /api/productos
-exports.newProduct=async(req,res,next)=>{
+exports.newProduct=catchAsyncErrors(async(req,res,next)=>{
     const product= await producto.create(req.body);
 
     res.status(201).json({
         success:true,
         product
     })
-}
-
+})
 
 //HABLEMOS DE FETCH
 //Ver todos los productos
@@ -96,6 +86,7 @@ function verProductos(){
     .then(res=>console.log(res))
     .catch(err=>console.error(err))
 }
+
 //verProductos(); LLamamos al metodo creado para probar la consulta
 
 //Ver por id
@@ -106,5 +97,5 @@ function verProductoPorID(id){
     .catch(err=>console.error(err))
 }
 
-//verProductoPorID('63463c2d06f21d81c9b50e95'); Probamos el metodo con un id
+//verProductoPorID('63456a8d9163cb9dbbcaa235'); Probamos el metodo con un idductoPorID('63463c2d06f21d81c9b50e95'); Probamos el metodo con un id
 
